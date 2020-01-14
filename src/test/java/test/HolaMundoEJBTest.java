0980fea627dbb189e4045a8f8bd403665bc90983
Steven Sanchez MAC <stevensanchez@ingenieros.com>
2020-01-14 11:13:54 -0500

    Project EJB

---
+++ src/test/java/test/HolaMundoEJBTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import beans.HolaMundoEJB;
+import static org.junit.Assert.*;
+import javax.ejb.embeddable.EJBContainer;
+import javax.naming.Context;
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+/**
+ *
+ * @author macbook
+ */
+public class HolaMundoEJBTest {
+    
+    
+    private static EJBContainer contenedor;
+    private static Context contexto;
+    private static HolaMundoEJB ejb;
+    
+    @Before
+    public void inicirContenedor() throws Exception{
+        System.out.println("Iniciando EJB container");
+        contenedor = EJBContainer.createEJBContainer();
+        contexto = contenedor.getContext();
+        ejb = (HolaMundoEJB) contexto.lookup("java:global/classes/HolaMundoEJB!beans.HolaMundoEJB");
+        
+    }
+    
+    @Test
+    public void testAddNumbers() throws Exception{
+        int dato1 = 3;
+        int dato2 = 5;
+        int resultado = ejb.sumar(dato1, dato2);
+        assertEquals((dato1+dato2), resultado);
+         System.out.println("\nOperacion terminada, resultado: "+resultado+ "\n");
+        
+        
+        
+    }
+    
+    
+    
+    
+    public HolaMundoEJBTest() {
+    }
+    
+}
